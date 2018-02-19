9669f12bd8fd363bbe70b954078b8422260a31fb
Josiah May <josiahm@rams.colostate.edu>
2018-02-19 16:45:26 -0700

    Basic Setup

--- src/cs455/scaling/util/CommandLineParser.java
+++ src/cs455/scaling/util/CommandLineParser.java
@@ -6,6 +6,12 @@ public class CommandLineParser {
 
   public enum Program {Server, Client}
 
+  private static final String serverCommandLine = "java cs455.scaling.server.Server "
+      + "portnum thread-pool-size";
+  private static final String clientCommandLine = "java cs455.scaling.client.Client "
+      + "server-host server-port message-rate";
+
+
   public static ArgsReturned verifyArgs(String[] args, Program type){
 
     switch (type){
@@ -20,71 +26,57 @@ public class CommandLineParser {
 
   private static ArgsReturned getSeverArgs(String[] args) {
 
-    if (args.length != 2) {
-      System.err.println("Invalid number of arguments: " + args.length + " expected 2");
-      System.err.println("java cs455.scaling.server.Server portnum thread-pool-size");
-      System.exit(1);
-    }
+    checkArgsLength(args.length, 2);
 
-    int portNumber = -1;
-    int threadPool = -1;
-    try {
-      portNumber = validInt(args[0]);
-    } catch (IllegalArgumentException e){
-      System.out.println("The port number argument is invalid\n" + e.getMessage());
-      System.exit(1);
-    }
+    int portNumber = validInt(args[0],"The port number argument is invalid\n");
+    int threadPool = validInt(args[1], "The thread pool number argument is invalid\n");
 
-    try {
-      threadPool = validInt(args[1]);
-    } catch (IllegalArgumentException e){
-      System.out.println("The thread pool number argument is invalid\n" + e.getMessage());
-      System.exit(1);
-    }
 
     return new ServerArgs(portNumber, threadPool);
   }
 
-  private static ArgsReturned getClientArgs(String[] args) {
-
-    if (args.length != 3) {
-      System.err.println("Invalid number of arguments: " + args.length + " expected 3");
-      System.err.println("java cs455.scaling.client.Client server-host server-port message-rate");
+  private static void checkArgsLength(int receivedLength, int expectedLength) {
+    if (receivedLength != expectedLength) {
+      System.err.println("Invalid number of arguments: " + receivedLength
+          + " expected " + expectedLength);
+      if(expectedLength == 2){
+        System.err.println(serverCommandLine);
+      }
+      if(expectedLength == 3){
+        System.err.println(clientCommandLine);
+      }
       System.exit(1);
     }
-
-    String hostIp = args[0];
-    int hostPort = -1;
-    int mesageRate = -1;
-
-    try {
-      hostPort = validInt(args[1]);
-    } catch (IllegalArgumentException e){
-      System.out.println("The host port number argument is invalid\n" + e.getMessage());
-      System.exit(1);
-    }
-
-    try {
-      mesageRate = validInt(args[2]);
-    } catch (IllegalArgumentException e){
-      System.out.println("The message rate number argument is invalid\n" + e.getMessage());
-      System.exit(1);
-    }
-
-    return new ClientArgs(hostIp, hostPort, mesageRate);
   }
 
-  private static int validInt(String number){
-    int rt;
+  private static ArgsReturned getClientArgs(String[] args) {
+
+    checkArgsLength(args.length, 3);
+
+    String hostIp = args[0];
+
+    int hostPort = validInt(args[1], "The host port number argument is invalid\n");
+    int messageRate = validInt(args[2], "The message rate number argument is invalid\n" );
+
+
+    return new ClientArgs(hostIp, hostPort, messageRate);
+  }
+
+  private static int validInt(String number, String message){
+    int rt = -1;
     try {
       rt = Integer.parseInt(number);
+      if(rt <= 0){
+        invalidArgument(message+ "The number is less then 0");
+      }
     } catch (NumberFormatException e){
-      throw new IllegalArgumentException("The argument is not a number.");
-    }
-    if(rt <= 0){
-      throw new IllegalArgumentException("The number is less then 0");
+      invalidArgument(message + "The argument is not a number.");
     }
     return rt;
   }
 
+  private static void invalidArgument(String message){
+    System.err.println(message);
+    System.exit(1);
+  }
 }
