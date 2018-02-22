9fe2eea55feb39bf8d094210964cca84210084d8
Josiah May <josiahm@rams.colostate.edu>
2018-02-22 16:54:36 -0700

    Comments for server args

--- src/cs455/scaling/util/ServerArgs.java
+++ src/cs455/scaling/util/ServerArgs.java
@@ -1,10 +1,24 @@
 package cs455.scaling.util;
 
+/**
+ * Wrapper class for the args for starting a server
+ */
 public class ServerArgs extends ArgsReturned {
 
+  /**
+   * The port number you want your server to run on
+   */
   public final int portNumber;
+  /**
+   * The number of threads in your thread pool
+   */
   public final int threadPoolSize;
 
+  /**
+   * Constructor that sets the port number and thread pool size
+   * @param port The port number
+   * @param pool The thread pool size
+   */
   ServerArgs(int port, int pool) {
     this.portNumber = port;
     this.threadPoolSize = pool;
