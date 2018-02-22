82d7bdda304c9c421eb5dc8873bd801cfa8b123e
Josiah May <josiahm@rams.colostate.edu>
2018-02-22 16:59:01 -0700

    Comments for client args

--- src/cs455/scaling/util/ClientArgs.java
+++ src/cs455/scaling/util/ClientArgs.java
@@ -1,11 +1,30 @@
 package cs455.scaling.util;
 
+/**
+ * Wrapper class for the args for a client
+ */
 public class ClientArgs extends ArgsReturned{
 
+  /**
+   * The IP address of the server
+   */
   public final String hostIpAddress;
+  /**
+   * The port number the server is listening on
+   */
   public final int hostPortNumber;
+  /**
+   * How fast messages should be sent to the server
+   */
   public final int messageRate;
 
+  /**
+   * Constructor that sets the server IP address and port number
+   * and the message rate of the client
+   * @param ip IP address of the server
+   * @param port  port number of the server
+   * @param rate message rate for the client
+   */
   ClientArgs(String ip, int port, int rate){
     this.hostIpAddress = ip;
     this.hostPortNumber = port;
