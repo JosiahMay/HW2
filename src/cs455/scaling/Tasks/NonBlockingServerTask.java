577e22e66ed5de99eeb7c34e005dece239977e3c
Josiah May <josiahm@rams.colostate.edu>
2018-03-04 15:17:54 -0700

    Basic tasks for server

---
+++ src/cs455/scaling/Tasks/NonBlockingServerTask.java
@@ -0,0 +1,13 @@
+package cs455.scaling.Tasks;
+
+/**
+ * A NIO Server that starts a nonblocking server, and creates tasks the based on the selector keys
+ */
+public class NonBlockingServerTask extends Task{
+
+
+  @Override
+  public void startTask() {
+
+  }
+}
